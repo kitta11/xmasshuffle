d989ceaa022c9993abc013b0c719e8d618430247
Krisztina Parrag <parragk@bud-0109.local>
2019-11-11 14:51:26 +0100

    Updates

---
+++ precache-manifest.12da4a258e4dfdd387f4304375283388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa9207591994075b4e6078cce95211d",
+    "url": "/xmasshuffle/index.html"
+  },
+  {
+    "revision": "2db170ec1adb82884684",
+    "url": "/xmasshuffle/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e0bcc486cadc75113ef0",
+    "url": "/xmasshuffle/static/js/2.418949fb.chunk.js"
+  },
+  {
+    "revision": "2db170ec1adb82884684",
+    "url": "/xmasshuffle/static/js/main.af65ca55.chunk.js"
+  },
+  {
+    "revision": "52f580ea188027d0b8bf",
+    "url": "/xmasshuffle/static/js/runtime-main.11c89eb1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/xmasshuffle/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.699b50616e87e89cce6dbc62718c1d41.js"
+  "/xmasshuffle/precache-manifest.12da4a258e4dfdd387f4304375283388.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/xmasshuffle/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
